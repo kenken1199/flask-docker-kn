b13adbf16a908658c5a0d2e41e0eba574de68d06
kenta <nakamori.k@gmail.com>
2021-07-21 18:32:35 +0900

    CD

--- app/index.py
+++ app/index.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-  return "<h1>Jiro, Flask!</h1>"
+  return "<h1>korona, Flask!</h1>"
 
 if __name__ == "__main__":
   port = int(os.environ.get('PORT', 5000))
