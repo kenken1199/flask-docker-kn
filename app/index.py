1d6428aad6133bd0206a375a72df6148be99e3ae
kenken1199 <47364493+kenken1199@users.noreply.github.com>
2022-04-15 16:54:57 +0900

    Update index.py

--- app/index.py
+++ app/index.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-  return "<h1>from joyfull, Flask!</h1>"
+  return "<h1>from myhome, Flask!</h1>"
 
 if __name__ == "__main__":
   port = int(os.environ.get('PORT', 5000))
